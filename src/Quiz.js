1716ee81670f90afe15f8040de54d0b79b2287ed
SunakshiChagti <sunakshichagti@gmail.com>
2018-11-19 15:16:13 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,14 +15,14 @@ class Quiz extends Component{
     }
     render()
     {
-        const isQuizEnd = true
+        const isQuizEnd = ((this.state.quiz_position -1 ) === quizData.quiz_questions.length);
         return(
             <div>
-                {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+                {isQuizEnd = ? <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
             </div>
         )
     }
 }
 
-export default Quiz
+export default Quiz;
